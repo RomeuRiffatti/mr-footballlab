9cc04de6a40b748d6659eef7be1984de5525f0fc
romeu <romeufftt@gmail.com>
2025-04-28 17:47:07 -0300

    add api no pah

--- frontend/src/endpoints/api.tsx
+++ frontend/src/endpoints/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://discerning-charisma-production.up.railway.app"
+export const BASE_URL = "https://discerning-charisma-production.up.railway.app/api"
 
 
 
