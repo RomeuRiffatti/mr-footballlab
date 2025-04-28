fa87b28780c8a9b45da4c3a42a042e3438fcc839
romeu <romeufftt@gmail.com>
2025-04-28 16:22:22 -0300

    media root

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -154,7 +154,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = '/var/lib/postgresql/data'
+MEDIA_ROOT = BASE_DIR / 'media' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
