33b387d705912ba09ee31e8df9b2c4b62393fcad
romeu <romeufftt@gmail.com>
2025-05-10 16:37:51 -0300

    adicionando validação de webhook

--- backend/api/apimp.py
+++ backend/api/apimp.py
@@ -5,8 +5,12 @@ from rest_framework import status
 from rest_framework.permissions import  AllowAny
 import mercadopago
 import os
+       
+import hashlib
+import hmac
+import urllib.parse
 
-
+secret = '760086e9d48b8343c499e7bffdc8f03152d214b7d5214538ec25777450c88ec8'
 
 
 @api_view(['POST'])
@@ -86,7 +90,54 @@ def create_mercado_pago_preference(request):
 api_view(['POST'])
 @permission_classes([AllowAny])
 def get_webhook(request):
-    order = Order.objects.all().first()
-    order.status = 'Done'
-    order.save()
-    return (Response('Rota acessada com sucesso!', status=status.HTTP_201_CREATED))
+ 
+
+    # Obtain the x-signature value from the header
+    xSignature = request.headers.get("x-signature")
+    xRequestId = request.headers.get("x-request-id")
+
+    # Obtain Query params related to the request URL
+    queryParams = urllib.parse.parse_qs(request.url.query)
+
+    # Extract the "data.id" from the query params
+    dataID = queryParams.get("data.id", [""])[0]
+
+    # Separating the x-signature into parts
+    parts = xSignature.split(",")
+
+    # Initializing variables to store ts and hash
+    ts = None
+    hash = None
+
+    # Iterate over the values to obtain ts and v1
+    for part in parts:
+        # Split each part into key and value
+        keyValue = part.split("=", 1)
+        if len(keyValue) == 2:
+            key = keyValue[0].strip()
+            value = keyValue[1].strip()
+            if key == "ts":
+                ts = value
+            elif key == "v1":
+                hash = value
+
+    # Obtain the secret key for the user/application from Mercadopago developers site
+    secret = secret
+
+    # Generate the manifest string
+    manifest = f"id:{dataID};request-id:{xRequestId};ts:{ts};"
+
+    # Create an HMAC signature defining the hash type and the key as a byte array
+    hmac_obj = hmac.new(secret.encode(), msg=manifest.encode(), digestmod=hashlib.sha256)
+
+    # Obtain the hash result as a hexadecimal string
+    sha = hmac_obj.hexdigest()
+    if sha == hash:
+        order = Order.objects.all().first()
+        order.status = 'Done'
+        order.save()
+        return (Response('Rota acessada com sucesso!', status=status.HTTP_201_CREATED)) ##funcionando
+    else:
+        return (Response('Não validado', status=status.HTTP_400_BAD_REQUEST)) ##funcionando  
+
+
